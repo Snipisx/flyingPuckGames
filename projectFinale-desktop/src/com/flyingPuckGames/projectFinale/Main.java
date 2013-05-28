a89c2fd20c73d7c046efa0b1f0d0c1ca11201bb6
AKIRAWR <diiiiewasd@gmail.com>
2013-05-29 00:48:22 +0200

    Updated libgdx libs, preparing merge from branch menus.

--- projectFinale-desktop/src/com/flyingPuckGames/projectFinale/Main.java
+++ projectFinale-desktop/src/com/flyingPuckGames/projectFinale/Main.java
@@ -2,12 +2,14 @@ package com.flyingPuckGames.projectFinale;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.graphics.Texture;
 
 public class Main {
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "projectFinale - " + MegaGame.VERSION ;
 		cfg.useGL20 = true;
+		cfg.resizable = false;
 		cfg.width = 1100;
 		cfg.height = 600;
 		
